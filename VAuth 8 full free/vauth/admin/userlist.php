405bf010960e50b7f7af2736e27daaa723f81050
Alex Kanunnikov <lifeart92@gmail.com>
2013-09-25 11:12:26 +0400

    Page size reduce

--- VAuth 8 full free/vauth/admin/userlist.php
+++ VAuth 8 full free/vauth/admin/userlist.php
@@ -19,21 +19,16 @@ $tag = '';
 			if (!empty($_GET['del_user']) and is_numeric($_GET['del_user']) ) {
 			
 				$id = abs(intval($_GET['del_user']));
-	
 				$row_deluser = $db->super_query( "SELECT user_id, user_group, name, foto FROM " . USERPREFIX . "_users WHERE user_id='$id'" );
-
-				if( ! $row_deluser['user_id'] ) die( "User not found" );
-
-				if ($row_deluser['user_group'] <= 1 ) die( $lang['user_undel'] );
-
+				if(!$row_deluser['user_id']) die( "User not found" );
+				if ($row_deluser['user_group'] <= 1) die("Not allowed Group");
+				
 				$db->query( "DELETE FROM " . USERPREFIX . "_pm WHERE user_from = '{$row_deluser['name']}' AND folder = 'outbox'" );
-				
-				@unlink( ROOT_DIR . "/uploads/fotos/" . $row_deluser['foto'] );
-				
-				$db->query( "delete FROM " . USERPREFIX . "_users WHERE user_id='$id'" );
-				$db->query( "delete FROM " . USERPREFIX . "_banned WHERE users_id='$id'" );
-				$db->query( "delete FROM " . USERPREFIX . "_pm WHERE user='$id'" );
+				$db->query( "DELETE FROM " . USERPREFIX . "_users WHERE user_id='$id'" );
+				$db->query( "DELETE FROM " . USERPREFIX . "_banned WHERE users_id='$id'" );
+				$db->query( "DELETE FROM " . USERPREFIX . "_pm WHERE user='$id'" );
 				$db->query( "INSERT INTO " . USERPREFIX . "_admin_logs (name, date, ip, action, extras) values ('".$db->safesql($member_id['name'])."', '{$_TIME}', '{$_IP}', '65', '{$row_deluser['name']}')" );
+				@unlink( ROOT_DIR . "/uploads/fotos/" . $row_deluser['foto'] );
 				
 				header('Location: ./'.$admin_php_name.'?mod=vauth&page=users');
 			}
@@ -85,24 +80,18 @@ $tag = '';
 
 				}
 				if( ! empty( $_GET['sex'] ) ) {
-					
 					$getparams[] = 'sex='.$_GET['sex'];
-					
 					switch($_GET['sex']) {
-					
 						case 'male': $where[] = 'sex = \''.$vauth_text[4].'\''; break;
 						case 'female': $where[] = 'sex = \''.$vauth_text[5].'\''; break;
-						default : $where[] = 'sex !=  ""'; break;
-					
+						default : $where[] = 'sex !=  ""'; break;			
 					}
-					
 				}
 				if( ! empty( $_GET['friends'] ) and  $_GET['friends'] == 'yes') {
 					$where[] = 'userfriends !=  ""';
 					$getparams[] = 'friends='.$_GET['friends'];
 				}
 				if( ! empty( $_GET['vis'] ) ) {
-				
 					$getparams[] = 'vis='.$_GET['vis'];
 					$vis24 = intval($_GET['vis']);
 					if (is_numeric($vis24)) $vis24 = $vis24*60*60; else die('Error');
@@ -114,7 +103,6 @@ $tag = '';
 					$where[] = '( fs_connected = \'1\' or go_connected = \'1\' or ma_connected = \'1\' or ms_connected = \'1\' or in_connected = \'1\' or gh_connected = \'1\' or od_connected = \'1\' or vk_connected = \'1\' or fb_connected = \'1\' or tw_connected = \'1\' or st_connected = \'1\' )';
 				}
 				if( ! empty( $_GET['reg'] ) ) {
-				
 					$getparams[] = 'reg='.$_GET['reg'];
 					$reg24 = intval($_GET['reg']);
 					if (is_numeric($reg24)) $reg24 = $reg24*60*60; else die('Error');
@@ -133,7 +121,6 @@ $tag = '';
 					$where[] = "email like '$search_mail%'";
 				}
 				if( ! empty( $_GET['search_banned'] ) ) {
-				
 					$getparams[] = 'search_banned='.$_GET['search_banned'];
 					if ($_GET['search_banned'] == 'yes') {
 						$search_banned = $db->safesql( $_GET['search_banned'] );
@@ -147,54 +134,45 @@ $tag = '';
 					$where[] = "reg_date>='" . strtotime( $fromregdate ) . "'";
 				}
 				if( ! empty( $_GET['toregdate'] ) ) {
-				
 					$getparams[] = 'toregdate='.$_GET['toregdate'];
 					$toregdate = $_GET['toregdate'];
 					$where[] = "reg_date<='" . strtotime( $toregdate ) . "'";
 				}
 				if( ! empty( $_GET['fromentdate'] ) ) {
-					
 					$getparams[] = 'fromentdate='.$_GET['fromentdate'];
 					$fromentdate = $_GET['fromentdate'];
 					$where[] = "lastdate>='" . strtotime( $fromentdate ) . "'";
 				}
 				if( ! empty( $_GET['toentdate'] ) ) {
-				
 					$getparams[] = 'toentdate='.$_GET['toentdate'];
 					$toentdate = $_GET['toentdate'];
 					$where[] = "lastdate<='" . strtotime( $toentdate ) . "'";
 				}
 				if( ! empty( $_GET['search_news_f'] ) ) {
-				
 					$getparams[] = 'search_news_f='.$_GET['search_news_f'];
 					$search_news_f = $_GET['search_news_f'];
 					$search_news_f = intval( $search_news_f );
 					$where[] = "news_num>='$search_news_f'";
-					
 				}
 				if( ! empty( $_GET['search_news_t'] ) ) {
-					
 					$getparams[] = 'search_news_t='.$_GET['search_news_t'];
 					$search_news_t = $_GET['search_news_t'];
 					$search_news_t = intval( $search_news_t );
 					$where[] = "news_num<'$search_news_t'";
 				}
 				if( ! empty( $_GET['search_coms_f'] ) ) {
-				
 					$getparams[] = 'search_coms_f='.$_GET['search_coms_f'];
 					$search_coms_f = $_GET['search_coms_f'];
 					$search_coms_f = intval( $search_coms_f );
 					$where[] = "comm_num>='$search_coms_f'";
 				}
 				if( ! empty( $_GET['search_coms_t'] ) ) {
-				
 					$getparams[] = 'search_coms_t='.$_GET['search_coms_t'];
 					$search_coms_t = $_GET['search_coms_t'];
 					$search_coms_t = intval( $search_coms_t );
 					$where[] = "comm_num<'$search_coms_t'";
 				}
 				if( ! empty( $_GET['search_reglevel'] ) ) {
-				
 					$getparams[] = 'search_reglevel='.$_GET['search_reglevel'];
 					$search_reglevel = intval( $_GET['search_reglevel'] );
 					if ($_GET['search_reglevel'] <= 1) $where[] = "user_group>='1'";
@@ -202,7 +180,6 @@ $tag = '';
 				}
 				
 				$where[] = 'userpassword_hash!=""';
-				
 				$where = implode( " AND ", $where );
 				$getparams = implode( "&", $getparams );
 				if( ! $where ) $where = "user_group >= '2' and userpassword_hash!=\"\"";
@@ -211,13 +188,11 @@ $tag = '';
 				
 				if (!empty($_GET['start_num'])) {
 				
-					$start_num = intval($_GET['start_num']);
-				
+					$start_num = abs(intval($_GET['start_num']));				
 					$limit_from = $start_num;
 					$limit_to = $row_per_page;
 				
 				}
-				//$db->query("SET NAMES utf8");
 				
 $connect_sql_login = $db->query( "SELECT * FROM " . USERPREFIX . "_users where $where ORDER BY  `user_id` $sort_type LIMIT $limit_from , $limit_to" );	
 
