0a719d4d807b1c5fd6ca903f29de6aaaa42738f3
Alex Kanunnikov <lifeart92@gmail.com>
2014-01-08 14:44:31 +0400

    Ukrainian langfiles
    by http://vk.com/pan_oleksandr (Олександр Шибіка)

---
+++ VAuth 8 full free/vauth/langfiles/ukrainian.php
@@ -0,0 +1,285 @@
+<?PHP
+
+	// ** Адмінка
+	
+		$vauth_text['admin_menu_info'] = 'Інформація';
+		$vauth_text['admin_menu_users'] = 'Користувачі';
+		$vauth_text['admin_menu_settings'] = 'Налаштування';
+		$vauth_text['all_connected'] = 'Всього підключили соціальні мережі: ';
+		$vauth_text['male_count'] = 'Число чоловіків: ';
+		$vauth_text['female_count'] = 'Число жінок: ';
+		$vauth_text['all_vauth_reg'] = 'Зареєстровано через VAuth (всього): ';
+		$vauth_text['all_vk_reg'] = 'Зареєстровано через Вконтакте: ';
+		$vauth_text['all_fb_reg'] = 'Зареєстровано через Facebook: ';
+		$vauth_text['all_tw_reg'] = 'Зареєстровано через Twitter: ';
+		$vauth_text['all_with_friends'] = 'Мають друзів на сайті : ';
+		
+		// ** Конфігурація модулю
+		$vauth_text['site_url'] = 'Адреса сайту:';
+		$vauth_text['mod_lang'] = 'Мова модулю:';
+		$vauth_text['send_pm_on_reg'] = 'Відправляти персональне повідомлення при реєстрації:';
+		$vauth_text['yes'] = 'Так';
+		$vauth_text['no'] = 'Ні';
+		
+		$vauth_text['vkontakte_app_id'] = 'vkontakte app id:';
+		$vauth_text['vkontakte_app_secret'] = 'vkontakte app secret:';
+		$vauth_text['facebook_app_id'] = 'facebook app id:';
+		$vauth_text['facebook_app_secret'] = 'facebook app secret:';
+		$vauth_text['twitter_app_id'] = 'twitter app id:';
+		$vauth_text['twitter_app_secret'] = 'twitter app secret:';
+
+		
+		$vauth_text['vk_usergroup'] = 'Група користувачів з Вконтакте:';
+		$vauth_text['fb_usergroup'] = 'Група користувачів з facebook:';
+		$vauth_text['tw_usergroup'] = 'Група користувачів з twitter:';
+		$vauth_text['login_request'] = 'Запитувати бажаний логін при реєстрації:';
+		$vauth_text['email_request'] = 'Запитувати e-mail при реєстрації:';
+		$vauth_text['password_request'] = 'Запитувати бажаний пароль при реєстрації:';
+		$vauth_text['avatar_size'] = 'Розмір аватарки по більшій стороні (px):';
+		$vauth_text['square_hor_shift'] = 'Центрування широких зображень:';
+		$vauth_text['square_vert_shift'] = 'Центрування високих зображень:';
+		$vauth_text['update_period'] = 'Час оновлення друзів в секундах:';
+		$vauth_text['update_settings'] = 'Оновити налаштування';
+		$vauth_text['support_group'] = 'Спільнота підтримки модуля Вконтакте.';
+		$vauth_text['hellopage'] = '
+			аскаво просимо в Адмін-панель модулю <b>VAuth 8</b>, <br/>
+			скористайтеся меню зверху для отримання інформації, яка вас цікавить та налаштування модулю.
+			<br/><br/>
+			<b>VirtualAuth</b> - новий проект авторизації користувачів чрез соціальні мережі в популярних CMS.</br>
+			На даний момент VAuth підтримує авторизацію через такі соціальні мережі, як:</br>
+			<b>vkontakte, facebook, twitter, odnoklassniki, foursquare, instagram, google+, мир@mail.ru, github, windows live</b></br>
+			Цей список постійно розширюється, так як ведеться активне просування модулю в мережі.
+			<br/><br/>
+			Я щиро сподіваюся, що використання даного модуля адміністраторами сайтів допоможе<br/>
+			підвищити безпеку авторизації, збільшити її швидкість і залучити нових користувачів на ресурс.<br/>
+			';
+	
+		// * VAuth 8
+		
+		$vauth_text['admin_email_auth'] = 'Дозволити авторизацію при збігу e-mail?';
+		$vauth_text['all_in_reg'] = 'Зареєстровано через Instagram: ';		
+		$vauth_text['all_fs_reg'] = 'Зареєстровано через Foursquare: ';		
+		$vauth_text['all_go_reg'] = 'Зареєстровано через Google: ';		
+		$vauth_text['all_od_reg'] = 'Зареєстровано через Odnoklassniki: ';		
+		$vauth_text['all_fl_reg'] = 'Зареєстровано через Flickr: ';		
+		$vauth_text['all_gh_reg'] = 'Зареєстровано через Github: ';	
+		$vauth_text['all_ms_reg'] = 'Зареєстровано через Windows Live: ';	
+		$vauth_text['all_ma_reg'] = 'Зареєстровано через Мир@mail.ru: ';	
+		
+		$vauth_text['reg_24_users'] = 'Зареєстровано за 24 години: ';		
+		$vauth_text['vis_24_users'] = 'Заходили протягом доби: ';
+		
+		$vauth_text['reg_week'] = 'Зареєстровано за неділю: ';		
+		$vauth_text['vis_week'] = 'Заходили протягом неділі: ';
+		
+		$vauth_text['reg_month'] = 'Зареєстровано за місяць: ';		
+		$vauth_text['vis_month'] = 'Заходили протягом місяця: ';		
+		
+		$vauth_text['od_usergroup'] = 'Группа для odnoklassniki:';
+		$vauth_text['fs_usergroup'] = 'Группа для foursquare:';
+		$vauth_text['go_usergroup'] = 'Група користувачів з google:';
+		$vauth_text['in_usergroup'] = 'Група користувачів з instagram:';
+		$vauth_text['fl_usergroup'] = 'Група користувачів з flickr:';
+		$vauth_text['gh_usergroup'] = 'Група користувачів з github:';
+		$vauth_text['ms_usergroup'] = 'Група користувачів з microsoft:';
+		$vauth_text['ma_usergroup'] = 'Група користувачів з mail.ru:';
+		$vauth_text['vi_usergroup'] = 'Група користувачів з vimeo:';
+		
+		$vauth_text['foursquare_app_id'] = 'foursquare app id:';
+		$vauth_text['foursquare_app_secret'] = 'foursquare app secret:';
+		
+		$vauth_text['odnoklassniki_app_id'] = 'odnoklassniki app id:';
+		$vauth_text['odnoklassniki_app_secret'] = 'odnoklassniki app secret:';
+		$vauth_text['odnoklassniki_pub_key'] = 'odnoklassniki pub key:';
+		
+		$vauth_text['mail_app_id'] = 'mail app id:';
+		$vauth_text['mail_app_secret'] = 'mail секретний ключ:';
+		$vauth_text['mail_pub_key'] = 'mail приватний ключ:';		
+		
+		$vauth_text['google_app_id'] = 'google app id:';
+		$vauth_text['google_app_secret'] = 'google app secret:';
+		
+		$vauth_text['flickr_app_id'] = 'flickr Key:';
+		$vauth_text['flickr_app_secret'] = 'flickr Secret:';
+		
+		$vauth_text['vimeo_app_id'] = 'vimeo Consumer Key:';
+		$vauth_text['vimeo_app_secret'] = 'vimeo Consumer Secret:';
+		
+		$vauth_text['github_app_id'] = 'github Client ID:';
+		$vauth_text['github_app_secret'] = 'github Secret:';
+		
+		$vauth_text['microsoft_app_id'] = 'microsoft Client ID:';
+		$vauth_text['microsoft_app_secret'] = 'microsoft Client secret:';		
+		
+		$vauth_text['instagram_app_id'] = 'instagram CLIENT ID:';
+		$vauth_text['instagram_app_secret'] = 'instagram CLIENT SECRET:';
+		
+		$vauth_text['info_header'] = '<b>Статистика модулю VirtualAuth</b>';
+		$vauth_text['vauth_header'] = 'VAuth - модуль авторизації та реєстрації користувачів через соціальні мережі';
+		$vauth_text['vauth_header2'] = 'Інформація VirtualAuth:';
+		$vauth_text['chmod_error'] = 'Невірно виставлені права на файл /modules/vauth/settings/user_settings.php <br />
+										Для налаштування модулю необхідно виставити CHMOD "0666"';
+										
+		$vauth_text['userlist_style'] = 'Вигляд списку користувачів VAuth: ';
+		$vauth_text['userlist_style_avatar'] = 'Аватарки';
+		$vauth_text['userlist_style_list'] = 'Список';
+		$vauth_text['admin_user_profile'] = 'Подивитися через пошук користувачів';
+		$vauth_text['admin_user_del'] = 'Видалити користувача ';
+		$vauth_text['admin_user_del_site'] = ' з сайту ';
+		$vauth_text['admin_searchform_login'] = 'Логін:';
+		$vauth_text['admin_searchform_regdate'] = 'Дата реєстрації:';
+		$vauth_text['admin_searchform_lastdate'] = 'Дата останнього відвідування:';
+		$vauth_text['admin_searchform_email'] = 'E-mail:';
+		$vauth_text['admin_searchform_email_help'] = 'Введіть <b>E-Mail</b> учасника для пошуку.';
+		$vauth_text['admin_searchform_from'] = 'Від';
+		$vauth_text['admin_searchform_to'] = 'До';
+		$vauth_text['admin_searchform_ban'] = 'Забанений:';
+		$vauth_text['admin_searchform_group'] = 'Група:';
+		$vauth_text['admin_searchform_sort'] = 'Сортування:';
+		$vauth_text['admin_searchform_mod'] = 'Відображення:';
+		$vauth_text['admin_searchform_usex'] = 'Стать користувача:';
+		$vauth_text['admin_searchform_site'] = 'Соціальна мережа:';
+		$vauth_text['admin_searchform_isfriend'] = 'Наявність друзів:';
+		$vauth_text['admin_searchform_regtime'] = 'Період реєстрації:';
+		$vauth_text['admin_searchform_nd'] = 'Нові внизу';
+		$vauth_text['admin_searchform_nu'] = 'Нові вгорі';
+		$vauth_text['admin_searchform_lis'] = 'Як у налаштуваннях';
+		$vauth_text['admin_searchform_list'] = 'Список';
+		$vauth_text['admin_searchform_avatar'] = 'Аватарки';
+		$vauth_text['admin_searchform_wfr'] = 'З Друзями';
+		$vauth_text['admin_searchform_allt'] = 'За весь час';
+		$vauth_text['admin_searchform_24h'] = 'За 24 години';
+		$vauth_text['admin_searchform_168h'] = 'За неделю';
+		$vauth_text['admin_searchform_720h'] = 'За месяць';
+		$vauth_text['admin_searchform_find'] = 'Знайти';
+		$vauth_text['admin_searchform_back'] = 'Повернути';
+		$vauth_text['admin_searchform_clean'] = 'Очистити';
+		$vauth_text['admin_searchform_nm'] = 'Не має значення';
+		$vauth_text['admin_searchform_group_all'] = '- Всі -';
+		$vauth_text['admin_searchform_allsites'] = 'Всі мережі';
+		$vauth_text['admin_searchform_pubnum'] = 'Кількість публікацій:';
+		$vauth_text['admin_searchform_comnum'] = 'Кількість коментарів:';
+		$vauth_text['admin_searchform_date_pick'] = 'Вибір дати за допомогою календаря';
+		$vauth_text['admin_searchform_help1'] = 'За стандартом в списку користувачів виводяться тільки користувачі зі статусом не менше журналіста. Для перегляду списку всіх учасників залиште поле для пошуку порожнім.';
+		$vauth_text['admin_menu_users'] = '<h3>Користувачі VAuth</h3>Список користувачів, зареєстрованих на сайті через VAuth, редагування їх акаунтів';
+		$vauth_text['admin_menu_settings'] = '<h3>Налаштування VAuth</h3>Налаштування загальних параметрів модулю, редагування розміру аватарок користувачів, даних соціальних мереж і груп користувачів';
+		$vauth_text['admin_menu_info'] = '<h3>Статистика модулю</h3>Відображення кількості зареєстрованих користувачів через соціальні мережі, їх стать, співвідношення';
+		$vauth_text['admin_menu_vauthinfo'] = '<h3>Інформація про модуль</h3>Опис роботи модулю, контактна інформація, спільнота вконтакті';
+		$vauth_text['admin_update_info'] = 'Оновити інформацію про ';
+	// ** Адмінка
+	
+	
+	// ** Модуль
+	
+		$vauth_text[0]	='Інформація оновиться через ';
+		$vauth_text[1]	='Покищо нікого немає..';
+		$vauth_text[2]	='1.2.1908 (Незазначена)';
+		$vauth_text[3]	='Білі кролики бігали по пухнастому снігу колами.';
+		$vauth_text[4]	='Чоловіча';
+		$vauth_text[5]	='Жіноча';
+		$vauth_text[6]	='Невідома';
+		$vauth_text[7]	=' хвилин.';
+		$vauth_text[8]	='Ви обрали управління аккаунтами соціальних мереж на сайті';
+		$vauth_text[9]	='Привіт, ';
+		$vauth_text[10]	='Під’єднати vkontakte';
+		$vauth_text[11]	='Від’єднати vkontakte';
+		$vauth_text[12]	='Під’єднати facebook';
+		$vauth_text[13]	='Від’єднати facebook';
+		$vauth_text[141]='Під’єднати twitter';
+		$vauth_text[142]='Від’єднати twitter';
+		$vauth_text[14]	='Цей аккаунт вже під’єднаний до користувача ';
+		$vauth_text[15]	='Такий аккаунт вже окремо зареєстрований з ім’ям користувача ';
+		$vauth_text[16]	='Щось трапилось.. поверніться, будь ласка, назад';
+		$vauth_text[17]	='Сполучення аккаунтів, зміна паролю';
+		$vauth_text[18]	='</b> <br /> Ваш новий DLE пароль: <b>';
+		$vauth_text[19]	='DLE VAuth module';
+		$vauth_text[20]	='Anonym, Please Login first!';
+		$vauth_text[21]	='Невідомо';
+
+		$vauth_text[22]	='Введіть бажаний нік, або залишіть поле пустим, для встановлення ніка із ';
+		$vauth_text[23]	='Продовжити';
+		$vauth_text[24]	='Реєстрація на сайті, Ваш новий пароль.';
+		$vauth_text[25]	=', <br /> для успішної авторизації через соціальні мережі рекомендовано його не змінювати.';
+		$vauth_text[26]	='Ввійти';
+		$vauth_text[27]	='назад';
+		$vauth_text[28]	='Такий користувач з FB вже зареєстрований на нашому сайті';
+		$vauth_text[29]	='FB info Error, that sucks!';
+		$vauth_text[30]	='Такий користувач з TW вже зареєстрований на нашому сайті';
+		$vauth_text[31]	='Error VK DB update';
+		$vauth_text[32]	='New user from twitter';
+		$vauth_text[34]	='&#8592; Повернутися до профілю';
+		$vauth_text[35]	='Сполучення акаунтів соціальних мереж дозволяє вам заходити на наш сайт, що не запам&#39;ятовуючи ім&#39;я користувача та пароль.
+							<br/>&#8226;&nbsp;Для сполучення аккаунта соціальної мережі з вашим профілем на сайті натисніть на відповідну кнопку вище.
+							<br/>&#8226;&nbsp;Після успішної операції сполучення, ви зможете зайти на наш сайт клікнувши по кнопці авторизації даної соціальної мережі.
+							<br/>&#8226;&nbsp;Можна підключити кілька соціальних мереж, і натиснувши на будь-яку кнопку авторизації, ви опинитеся в своєму профілі.
+							<br/>&#8226;&nbsp;Повторне натискання на кнопку відключить авторизацію на сайті через дану соціальну мережу.
+							';
+		$vauth_text[36]	= '</b> </br> </br> Для полегшення процесу авторизації на сайті <br/> не забудте підключити до вашого профілю <a href="/index.php?do=account_connect"><b>інші соціальні мережі</b></a> <br/><br/> А також рекомендується заповнити поле профілю "e-mail", для безпеки.';
+		$vauth_text[37] = '!</br></br> Ваш логін на сайті: <b>';
+		
+		$vauth_text['profile_vk'] = 'Вконтакті';
+		$vauth_text['profile_fb'] = 'facebook';
+		$vauth_text['profile_tw'] = 'twitter';
+		$vauth_text['loginform_login'] = 'Введіть бажаний логін на сайті:';
+		$vauth_text['loginform_email'] = 'Введіть ваш e-mail:';
+		$vauth_text['loginform_passw'] = 'Введіть бажаний пароль на сайті:';
+		$vauth_text['loginform_send'] = 'Зареєструватися!';
+		// ** VAuth 8
+		$vauth_text['fs_user_reg_errore']	='Такий користувач з foursquare вже зареєстрований на нашому сайті <br/> <a href="/"><b>Повернутися назад</b></a>';
+		$vauth_text['in_user_reg_errore']	='Такий користувач з instagram вже зареєстрований на нашому сайті <br/> <a href="/"><b>Повернутися назад</b></a>';
+		$vauth_text['go_user_reg_errore']	='Такий користувач з google+ вже зареєстрований на нашому сайті <br/> <a href="/"><b>Повернутися назад</b></a>';
+		$vauth_text['ms_user_reg_errore']	='Такий користувач з Windows Live вже зареєстрований на нашому сайті <br/> <a href="/"><b>Повернутися назад</b></a>';
+		$vauth_text['ma_user_reg_errore']	='Такий користувач з мир@mail.ru вже зареєстрований на нашому сайті <br/> <a href="/"><b>Повернутися назад</b></a>';
+		$vauth_text['gh_user_reg_errore']	='Такий користувач з github вже зареєстрований на нашому сайті <br/> <a href="/"><b>Повернутися назад</b></a>';
+		$vauth_text['profile_go'] = 'google+';
+		$vauth_text['profile_in'] = 'instagram';
+		$vauth_text['profile_fs'] = 'foursquare';
+		$vauth_text['profile_od'] = 'odnoklassniki';
+		$vauth_text['profile_ma'] = 'мир<font color="#faa61a">@</font>mail<font color="#faa61a">.ru</font>';
+		$vauth_text['profile_ms'] = 'Windows Live';
+		$vauth_text['profile_gh'] = 'github';
+		$vauth_text['en_go']='Підключити google+';
+		$vauth_text['en_gh']='Підключити github';
+		$vauth_text['en_ma']='Підключити мир@mail.ru';
+		$vauth_text['en_ms']='Підключити Windows Live';
+		$vauth_text['en_in']='Підключити instagram';
+		$vauth_text['en_fs']='Підключити foursquare';
+		$vauth_text['en_od']='Підключити odnoklassniki';
+		$vauth_text['di_go']='Відключити google+';
+		$vauth_text['di_in']='Відключити instagram';
+		$vauth_text['di_fs']='Відключити foursquare';
+		$vauth_text['di_od']='Відключити odnoklassniki';
+		$vauth_text['di_gh']='Відключити github';
+		$vauth_text['di_ma']='Відключити мир@mail.ru';
+		$vauth_text['di_ms']='Відключити Windows Live';
+		$vauth_text['email_error']='
+									 - Email allready exists, try to login with anower email </br>
+									Такий E-mail вже зареєстрований на нашому сайті, <br/>
+									підключіть соц. мережу в вашому профілі або скористайтеся відновленням паролю. <br/> <a href="/"><b>Повернутися назад</b></a>';
+									
+		$vauth_text['bad_email'] = ' Bad user email: ';
+		$vauth_text['bad_group'] = ' Не встановлена або невірно задана група користувача: ';
+		$vauth_text['vauth_reg_off'] = 'Реєстрація користувачів через VAuth заборонена';
+		$vauth_text['uid_error'] = 'Помилка отримання id користувача';
+		$vauth_text['vk_token_error'] = 'Не вдається отримати ключ авторизації вконтакте, можливо ви використовуєте застарілі дані <br/> <a href="/"><b>Повернутися назад</b></a>';
+		$vauth_text['fs_token_error'] = 'Не вдається отримати ключ авторизації foursquare, можливо ви використовуєте застарілі дані <br/> <a href="/"><b>Повернутися назад</b></a>';
+		$vauth_text['tw_token_error'] = 'Не вдається отримати ключ авторизації twitter, можливо ви використовуєте застарілі дані <br/> <a href="/"><b>Повернутися назад</b></a>';
+		$vauth_text['fb_token_error'] = 'Не вдається отримати ключ авторизації facebook, можливо ви використовуєте застарілі дані <br/> <a href="/"><b>Повернутися назад</b></a>';
+		$vauth_text['go_token_error'] = 'Не вдається отримати ключ авторизації google, можливо ви використовуєте застарілі дані <br/> <a href="/"><b>Повернутися назад</b></a>';
+		$vauth_text['od_token_error'] = 'Не вдається отримати ключ авторизації odnoklassniki, можливо ви використовуєте застарілі дані <br/> <a href="/"><b>Повернутися назад</b></a>';
+		$vauth_text['in_token_error'] = 'Не вдається отримати ключ авторизації instagram, можливо ви використовуєте застарілі дані <br/> <a href="/"><b>Повернутися назад</b></a>';
+		$vauth_text['gh_token_error'] = 'Не вдається отримати ключ авторизації github, можливо ви використовуєте застарілі дані <br/> <a href="/"><b>Повернутися назад</b></a>';
+		$vauth_text['ms_token_error'] = 'Не вдається отримати ключ авторизації Windows Live, можливо ви використовуєте застарілі дані <br/> <a href="/"><b>Повернутися назад</b></a>';
+		$vauth_text['ma_token_error'] = 'Не вдається отримати ключ авторизації мир@mail.ru, можливо ви використовуєте застарілі дані <br/> <a href="/"><b>Повернутися назад</b></a>';
+		$vauth_text['def_user'] = 'Merlin Monro ';
+		$vauth_text['def_login'] = 'user ';
+		$vauth_text['def_email'] = '@fakemail.com';
+		$vauth_text['def_pass'] = 'qwerty';
+		$vauth_text['fatal_reg_error'] = 'Matrix has you, Neo';
+		$vauth_text['no_auth_site'] = 'Не встановлений сайт авторизації, перевірте підтримку сесій сервером, обновіть сторінку <br/> <a href="/"><b>Повернутися назад</b></a>';
+		$vauth_text['no_function_oauth'] = 'У вашій збірці модуля не встановлена функція авторизації через ';
+		$vauth_text['error_oauth_ssid'] = 'Жахлива помилка авторизації! ID сесій авторизації не збігаються..  <br/> <a href="/"><b>Повернутися назад</b></a>';
+		$vauth_text['group'] = 'Група';
+		$vauth_text['registration'] = 'Реєстрація';
+	// ** Модуль
+?>
